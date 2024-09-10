5d19357a9c01294c24a8c2f6f0521666fab0ceef
toshan292003 <toshanyt@gmail.com>
2024-09-11 01:12:42 +0530

    gasgsag

--- Frontend/myapp/src/Pages/login.jsx
+++ Frontend/myapp/src/Pages/login.jsx
@@ -1,6 +1,7 @@
 import {React,useState,useEffect} from "react";
 import "./login.css";
 import LongLogo from "../Images/longLogo.png";
+import googleLogo from "../Images/google.png";
 
 export default function Login(){
     useEffect(() => {
@@ -15,9 +16,15 @@ export default function Login(){
                     <img src={LongLogo} alt="" />
                 </section>
                 <section>
-                    <a href="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/home">
-                        <button>Sign in with Google</button>
-                    </a>
+                    <div>
+                        <h2>Create a new account</h2>
+                        <a href="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/home">
+                            <img src={googleLogo} alt="" />
+                            <button>Sign up with Google</button>
+                        </a>
+                        <button className="loginbutton">Create an Account</button>
+                        <p>Already have an account? <span>Sign in</span></p>
+                    </div>
                 </section>
                 <section className="flex row center"><span>© 2023 Reachinbox. All rights reserved.</span></section>
             </div>
